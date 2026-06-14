52ec56ba1f6143364ba6671ff0be8d7c232e74cb
Kirill <kirillmatveev119@gmail.com>
2026-06-14 23:07:07 +0300

    da

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class PostController extends Controller
 {
     public function posts()
     {
-        return view('posts.index');
+        $posts = Post::orderBy('id', 'desc')->get();
+        return view('posts.index')->with('posts', $posts);
     }
 }
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,8 +1,12 @@
 @extends('layouts/main')
 @section('header-title')
 Posts
+
+
 @endsection()
 @section('content')
 Posts-POsTSSSS
-
+@foreach($posts as $el)
+<p>{{$el->title}}</p>
+@endforeach
 @endsection
